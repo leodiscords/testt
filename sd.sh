99074bb1a2a7ecef430c86512dfbc177450af9e3
leodiscords <166489239+leodiscords@users.noreply.github.com>
2024-05-29 20:30:50 -0400

    sds
    adad

---
+++ sd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt-cache policy docker-ce && sudo apt install docker-ce
+
+docker volume create portainer_data && docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+docker pull qmcgaw/gluetun && docker pull goodboy3/gaganode
+
+sudo ufw allow 443 && sudo ufw allow 36060
+
+sleep 15s && echo ' Done '
+
+/bin/bash
